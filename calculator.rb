fac24183348c360822917eeacb5352d60bd829ea
Mark Brooks <Markabrooks@gmail.com>
2018-01-03 15:53:32 -0700

    array contains expected output but output is nil

--- calculator.rb
+++ calculator.rb
@@ -1,16 +1,24 @@
 puts "Welcome to the Calculator!"
-   puts "Please calculate  . i.e '4 + 3 * 4'"
+    puts "Please calculate  . i.e '4 + 3 * 4'"
 
-   #gets user input and creates array for each value
-   cal = gets.chomp.split(" ")
+    #gets user input and creates array for each value
+    cal = gets.chomp.split(" ")
 
-   #creates a new array and converts string to integer or symbol
-   cal.map!.with_index do |num, i|
-     if i.even?
-       num.to_i
-     else
-       num.to_sym
-     end
+    #creates a new array and converts string to integer or symbol
+    cal.map!.with_index do |num, i|
+      if i.even?
+        num.to_i
+      else
+        num.to_sym
+      end
 
-   end
-   cal.inspect
+    end
+
+result = 0
+while cal.length >= 2
+  #recv, meth, arg = cal
+  result = cal[0].public_send(cal[1], cal[2])
+  cal.shift(3)
+  cal.unshift(result)
+
+end
