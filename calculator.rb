75df8f3eafcabf1aa26a21b1a74fd764cb576310
Mark Brooks <Markabrooks@gmail.com>
2018-01-03 20:33:35 -0700

    Remove Spacing Complete

--- calculator.rb
+++ calculator.rb
@@ -1,29 +1,21 @@
 puts "Welcome to the Calculator!"
-    puts "Please calculate  . i.e '4 + 3 * 4'"
+    puts "Please calculate  . i.e '4+3*4'"
 
     #gets user input and creates array for each value
-    cal = gets.chomp.split(" ")
-
-    #creates a new array and converts string to integer or symbol
-    cal.map!.with_index do |num, i|
-      if i.even?
-        num.to_i
-      else
-        num.to_sym
-      end
-
+    cal = gets.chomp.split(%r{(\+|\-|\/|\*)}).map do |x|
+      unless x =~ /(\+|\-|\/|\*)/ then x.to_i else x.to_sym end
     end
 
     result = 0
     while cal.length >= 2
       #recv, meth, arg = cal
-      result = cal[0].send(cal[1], cal[2])
+      result = cal[0].public_send(cal[1], cal[2])
       cal.shift(3)
       cal.unshift(result)
 
     end
 
-  final = result.to_i
-  print final
-  print "\n"
-  #true
+new = result.to_i
+print new
+print "\n"
+true
